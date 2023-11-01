fcd37d03e6c9652ce06f4111a4def356b3d20424
Masha Vulcu <97957872+mvulcu@users.noreply.github.com>
2023-11-01 12:06:33 +0100

    Update Chat_bot.py

--- Chat_bot.py
+++ Chat_bot.py
@@ -2,36 +2,36 @@ import telebot
 import random
 from telebot import types
 
-# Загружаем список интересных фактов
+# Load the list of interesting facts
 f = open('data/facts.txt', 'r', encoding='UTF-8')
 facts = f.read().split('\n')
 f.close()
-# Загружаем список поговорок
+# Load the list of sayings
 f = open('data/thinks.txt', 'r', encoding='UTF-8')
-thinks  = f.read().split('\n')
+thinks = f.read().split('\n')
 f.close()
-# Создаем бота
-bot = telebot.TeleBot('6285552342:AAFAdDtjz7O3QMWuFWWLhw5Ck8s8b-KZDAc')
-# Команда start
+# Create a bot
+bot = telebot.TeleBot('YOUR_BOT_TOKEN_HERE')  # Replace with your actual token
+# start command
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-        # Добавляем две кнопки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Facts")
-        item2=types.KeyboardButton("Sayings")
+        # Add two buttons
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Facts")
+        item2 = types.KeyboardButton("Sayings")
         markup.add(item1)
         markup.add(item2)
-        bot.send_message(m.chat.id, 'Click: \nFact for an interesting fact\nSaying for a wise quote',  reply_markup=markup)
-# Получение сообщений от юзера
+        bot.send_message(m.chat.id, 'Click: \nFact for an interesting fact\nSaying for a wise quote', reply_markup=markup)
+# Receiving messages from the user
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    # Если юзер прислал 1, выдаем ему случайный факт
+    # If the user sent 'Facts', we give them a random fact
     if message.text.strip() == 'Facts':
             answer = random.choice(facts)
-    # Если юзер прислал 2, выдаем умную мысль
+    # If the user sent 'Sayings', we give them a wise thought
     elif message.text.strip() == 'Sayings':
             answer = random.choice(thinks)
-    # Отсылаем юзеру сообщение в его чат
+    # We send the user a message back to their chat
     bot.send_message(message.chat.id, answer)
-# Запускаем бота
+# Run the bot
 bot.polling(none_stop=True, interval=0)
